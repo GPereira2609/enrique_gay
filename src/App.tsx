44e95c25cae0d7039b125b7e3b84723a9a095526
Gabriel <gabriel.pereira2609@gmail.com>
2024-02-21 18:35:03 -0300

    teste

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-orange-500 w-full'>
+      <div className='bg-orange-100 w-full ...'>Container</div>
+    </div>
+  )
+}
+
+export default App
